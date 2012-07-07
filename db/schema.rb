213e401ba1dea051de0d4a4e602a4dd704996c08
Catherine Leary Tomezsko <ctomezsko@gmail.com>
2012-07-07 18:18:55 -0400

    Finish sign in, second reading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120702003347) do
+ActiveRecord::Schema.define(:version => 20120707210234) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(:version => 20120702003347) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
