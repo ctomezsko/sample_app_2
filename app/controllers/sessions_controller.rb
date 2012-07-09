fdc3c08eb2ca5e4b1cd46ba8a5e1dca3c72734c6
Catherine Leary Tomezsko <ctomezsko@gmail.com>
2012-07-09 11:07:19 -0400

    Finish user edit, update, index, and destroy actions, second reading

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:session][:email])
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
@@ -19,4 +19,5 @@ class SessionsController < ApplicationController
     	sign_out
     	redirect_to root_path
   	end
+
 end
